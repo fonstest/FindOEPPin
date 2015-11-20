0e6b838098f7e333811e07209af052ccf5fdf466
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2015-11-20 23:12:24 +0100

    fixed analyzer
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- FindOEPPin/PinUnpackerDependencies/analyzer_2.py
+++ FindOEPPin/PinUnpackerDependencies/analyzer_2.py
@@ -6,7 +6,7 @@ ep_index = -1
 
 scale = 0.01 #scale factor to divide the write-sets size
 classes = []
-counter_list = [0] * 101 # 1 to 1 correspondence with the classes ( they are always 100 ) 
+counter_list = [0] * 100 # 1 to 1 correspondence with the classes ( they are always 100 ) 
 
 unique_write_set_index = []
 
@@ -16,46 +16,48 @@ def generate_classes(fraction,size):
 	cont = 0
 	classes.append([cont,int(fraction)])
 	cont = 1
+
 	acc = int(fraction)
 	acc = acc + fraction
+	
+	if acc == 0:
+		fraction = 1
+		acc = 1
+	
+	size = int(fraction * 1/scale)
 	#print "size is" + str(size)
+	
 	while acc < size:
+
 		classes.append([cont,int(acc)])
 		acc = acc + int(fraction)
 		cont = cont + 1
 
 	classes.append([cont,int(acc)])
+
 	#print classes
-	#print len(classes)
+	print "LEN OF CLASSES " + str(len(classes))
+	
 
-
-def insert_in_classes(delta_jmp,oep_flag):
+def insert_in_classes(delta_jmp,fraction,oep_flag):
     
 	global ep_index
 
-	if delta_jmp <= classes[0][1]:
-			counter_list[classes[0][0]] = counter_list[classes[0][0]] + 1
-			if oep_flag == 1:
-				ep_index = 0
-			return
+	index_calculated = delta_jmp // fraction
+	#print "Index calculated is " + str(index_calculated)
+	if index_calculated >= 1/scale:
+		index_calculated = 1/scale-1
 
-	if delta_jmp >= classes[100][1]:
-			counter_list[classes[100][0]] = counter_list[classes[100][0]] + 1
-			if oep_flag == 1:
-				ep_index = 100
-			return
-		
-	for i in range(len(classes)-1):
-		#print classes[i]
-		if delta_jmp >= classes[i][1] and delta_jmp < classes[i+1][1]:
-			counter_list[classes[i][0]] = counter_list[classes[i][0]] + 1
-			if oep_flag == 1:
-				ep_index = i
-			return
+	counter_list[int(index_calculated)] += 1
+	if oep_flag == 1:
+		ep_index = index_calculated
 
-			
+	#print "THE FIRST CLASS IS :" + str(classes[0][1]) +"\n"
+	#print "----------PUTTED " + str(delta_jmp) + " IN CLASS" + str(int(index_calculated)) + "------------\n"
 
 
+
+# ENTRY POINT			
 
 if len(sys.argv) != 2:
 	print "File name needed"
@@ -65,7 +67,6 @@ try:
 except IOError:
 	print "File not found"
 	sys.exit(0)
-
 
 
 
@@ -81,18 +82,18 @@ for line in in_file:
 	if witem_info not in unique_write_set_index:
 		unique_write_set_index.append(witem_info)
 
-#print str(unique_write_set_index) + "\n"
+print str(unique_write_set_index) + "\n"
 
 for witem in unique_write_set_index:
 
 	in_file.seek(0)
 	size = int(witem[1],10) # get the size from the witem 
 	index = witem[0] # get the index
-	fraction = size * scale 
+	fraction = int(size * scale)
 
 	generate_classes(fraction,size)
 
-	#print classes 
+	print classes 
 
 	for line in in_file:
 		splitted = line.strip().split(",") 
@@ -106,9 +107,9 @@ for witem in unique_write_set_index:
 			delta_jmp = int(splitted[2].strip()[12:],10)
 			#print delta_jmp
 			if oep == entry_point:
-				insert_in_classes(delta_jmp,1)
+				insert_in_classes(delta_jmp,fraction,1)
 			else:
-				insert_in_classes(delta_jmp,0)
+				insert_in_classes(delta_jmp,fraction,0)
 
 	#print "\n"
 	#print counter_list
@@ -117,8 +118,11 @@ for witem in unique_write_set_index:
 	print "Write set size: " + str(size) + "\n"
 
 	k=0
-	#print ep_index
+	print "EIP INDEX"  + str(ep_index)
 	for c in classes:
+		#print k
+		#print c[0]
+		#print "LAST CLASS IS" + str(classes[99])
 		if str(c[0]) == str(ep_index): 
 			print str(int(c[0])+1) +"/100 : " + str(counter_list[k]) + " <-- [x]oep here" + "\n"
 		else:
@@ -126,6 +130,9 @@ for witem in unique_write_set_index:
 
 		k = k+1
 
+
 	counter_list = [0] * 101
 	classes = []
 	ep_index = -1
+
+
